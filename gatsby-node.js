79255f7289fb3c9fb5e5fc942d7bac9c99a0213f
Alex Bussey <abussey@jwplayer.com>
2020-04-19 16:28:14 -0400

    Oops forgot something

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,7 +7,7 @@ exports.createPages = ({ graphql, actions }) => {
   return new Promise((resolve, reject) => {
     graphql(`
       {
-        allDatoCmsWork {
+        allDatoCmsForsale {
           edges {
             node {
               slug
@@ -16,12 +16,12 @@ exports.createPages = ({ graphql, actions }) => {
         }
       }
     `).then(result => {
-      result.data.allDatoCmsWork.edges.map(({ node: work }) => {
+      result.data.allDatoCmsForsale.edges.map(({ node: forsale }) => {
         createPage({
-          path: `works/${work.slug}`,
-          component: path.resolve(`./src/templates/work.js`),
+          path: `forsale/${forsale.slug}`,
+          component: path.resolve(`./src/templates/forsale.js`),
           context: {
-            slug: work.slug,
+            slug: forsale.slug,
           },
         })
       })
